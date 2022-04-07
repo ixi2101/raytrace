e2a74a713fa1ecde9d0b265a1054288a683e8099
ixi2101 <alpha@omega>
2022-04-07 22:44:27 +0100

    add ray at test

--- test/ray_test.cpp
+++ test/ray_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "ray.hpp"
+#include "vec3.hpp"
 
 
 TEST(Ray, ctor){
@@ -9,6 +10,15 @@ TEST(Ray, ctor){
     auto r = ray(orig,dir);    
     auto exp = orig.x();
     auto found = r.direction();
-    EXPECT_EQ(r.origin().x(), orig.x());
-    EXPECT_EQ(2,2);
+    EXPECT_TRUE(r.origin() == orig);
+    EXPECT_TRUE(r.direction() == dir);
+    
 }
+
+TEST(Ray, ray_at){
+    auto orig = point3{0,0,0};
+    auto dir = vec3{1,0,0};
+    auto r = ray(orig,dir); 
+    auto expected = point3{4,0,0};
+    EXPECT_TRUE(r.at(4) == expected);
+    }
