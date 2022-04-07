b0aa6e5ad61300ffbe989bfe7295d45b2c0f194c
ixi2101 <alpha@omega>
2022-04-07 16:50:15 +0100

    add progess indicator

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,21 @@ int main() {
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    
     for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\r Scanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            auto ir = static_cast<int>(255.999 * r);
+            auto ig = static_cast<int>(255.999 * g);
+            auto ib = static_cast<int>(255.999 * b);
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::cerr << "\nDone. \n";
 }
